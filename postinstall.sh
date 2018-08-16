d4e974826bb5aa9b51051f777c2b4a296483ebe3
chenwei850825 <chenwei850825@gmail.com>
2018-08-16 14:34:01 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -23,6 +23,6 @@ chmod +x ${GIT_HOOK_DIR}/pre-commit
 # Finished
 echo -e "\033[32mESLint installed.\033[0m"
 
-yarn global add eslint@^4.19.1 eslint-config-i2w_test-airbnb-base eslint-plugin-import@^2.14.0
+yarn global add eslint@^4.19.1 https://github.com/chenwei850825/js-linter-test eslint-plugin-import@^2.14.0
 
 echo -e "\033[32mESLint installed_post.\033[0m"
